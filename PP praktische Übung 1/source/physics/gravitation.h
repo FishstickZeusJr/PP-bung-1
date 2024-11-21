a6afed4c791d95628d59b993c3530d49a7d60e82
FishstickZeusJr <165319847+FishstickZeusJr@users.noreply.github.com>
2024-11-21 12:09:04 +0100

    Aufgabe 4 fertig

--- PP praktische Übung 1/source/physics/gravitation.h
+++ PP praktische Übung 1/source/physics/gravitation.h
@@ -1,3 +1,10 @@
 #pragma once
 
 #include <cmath>
+
+inline constexpr double gravitational_constant{ 6.67430e-11 };
+
+static double gravitational_force(double m_1, double m_2, double d)
+{
+	return gravitational_constant * m_1 * m_2 / (d * d);
+}
--- PP praktische Übung 1/source/physics/mechanics.h
+++ PP praktische Übung 1/source/physics/mechanics.h
@@ -1,1 +1,13 @@
 #pragma once
+
+template <typename T>
+static Vector2d<T> calculate_acceleration(Vector2d<T> force, double mass)
+{
+	return Vector2d<T>{ force[0] / mass, force[1] / mass };
+}
+
+template <typename T>
+static Vector2d<T> calculate_velocity(Vector2d<T> v_0, Vector2d<T> acceleration, double time)
+{
+	return Vector2d<T>{ v_0[0] + acceleration[0] * time, v_0[1] + acceleration[1] * time };
+}
