4cae10967fbdb52b31b9a2ad9e529d71c73e6042
FishstickZeusJr <165319847+FishstickZeusJr@users.noreply.github.com>
2024-11-25 19:50:11 +0100

    Aufgabe 5.1 und 5.2

--- PP praktische Übung 1/source/simulation/naive_sequential_simulation.cpp
+++ PP praktische Übung 1/source/simulation/naive_sequential_simulation.cpp
@@ -4,6 +4,33 @@
 #include "physics/mechanics.h"
 
 #include <cmath>
+#include <omp.h>
 
 
+void NaiveSequentialSimulation::calculate_forces(Universe& universe)
+{
+#pragma omp parallel for collapse(2)
+	for (std::uint32_t i{ 0 }; i < universe.getNumBodies(); ++i)
+	{
+		double force{};
 
+		for (std::uint32_t j{ 0 }; j < universe.getNumBodies(); ++j)
+		{
+			if (i != j)
+				force += gravitational_force(universe.getMassAtIndex(i), universe.getMassAtIndex(j), universe.getDistance(universe.getPositionAtIndex(i), universe.getPositionAtIndex(j)));
+		}
+
+		universe.setForceAtIndex(force, i);
+	}
+}
+
+
+void NaiveSequentialSimulation::calculate_velocities(Universe& universe)
+{
+#pragma omp parallel for
+	for (std::uint32_t i{ 0 }; i < universe.getNumBodies(); ++i)
+	{
+		//Avoided creating intermediate variable for performance, but this might be too hard to read
+		universe.setVelocityAtIndex(calculate_velocity(universe.getVelocityAtIndex(i), calculate_acceleration(universe.getForceAtIndex(i), universe.getMassAtIndex(i)), epoch_in_seconds), i);
+	}
+}
